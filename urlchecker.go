8438109547ee4ebf22abdefd2c97bc36d36f599c
ghkdwjddus <ghkdwjddus52@naver.com>
2022-05-27 01:49:01 +0000

    update go scrapper code

---
+++ urlchecker.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errReqsF = errors.New("Request Error")
+
+func main() {
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.naver.com/",
+		"https://www.google.com/",
+		"https://www.reddit.com/",
+		"https://www.amazon.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://www.airbnb.com/",
+	}
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errReqsF
+	}
+	return nil
+}
